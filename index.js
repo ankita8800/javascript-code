e645fbd38acf60d30c2edb9f0621baa3b68eedf6
ankita8800 <119149080+ankita8800@users.noreply.github.com>
2025-01-06 00:09:36 +0530

    varibles

---
+++ index.js
@@ -0,0 +1,33 @@
+// Varibles 
+// javascript declare varibles 4 ways
+// Automatically
+
+x=5;
+y=6;
+z=x+y;
+document.getElementById("addnumber"). textContent="The value is z is " + z;
+
+// Let
+let a=5;
+let b=6;
+let c=a+b;
+document.getElementById("addnumber"). textContent="The value is c is " + c;
+
+// const
+const d=5;
+const e=6;
+const f=a+b;
+document.getElementById("addnumber"). textContent="The value is f is " + f;
+
+// var is redeclare
+var x=5;
+var x=6;
+var z=x+x;
+document.getElementById("addnumber"). textContent="The value is z is " + z;
+
+
+// Airthemetic operators
+
+let result= 2 + 2 / 2 - 2 * 2;
+console.log(result);
+
