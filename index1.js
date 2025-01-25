e5d69bee5064a1183cef40d93620045804b3c147
ankita8800 <119149080+ankita8800@users.noreply.github.com>
2025-01-25 22:16:33 +0530

    calculator

--- index1.js
+++ index1.js
@@ -113,3 +113,21 @@ function convertTemperature() {
     console.log(i);
 }
 
+// calculator
+const display = document.getElementById('display');
+
+    function appendValue(value) {
+      display.value += value;
+    }
+
+    function clearDisplay() {
+      display.value = '';
+    }
+
+    function calculate() {
+      try {
+        display.value = eval(display.value);
+      } catch {
+        display.value = 'Error';
+      }
+    }
